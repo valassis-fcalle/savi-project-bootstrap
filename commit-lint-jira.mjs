8612bf01abcd1dc77e9c8e872e44a9a63b9b38cc
Francisco Calle <fcalle@valassis.es>
2022-06-29 08:51:01 +0200

    feat: configure commitlint with jira rules

---
+++ commit-lint-jira.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env zx
+
+import { $, cd, fs } from "zx";
+import { PROJECT_FOLDER } from "./utils/env.mjs";
+import { printStep } from "./utils/steps.mjs";
+
+cd(PROJECT_FOLDER);
+
+console.log(printStep(), "Installing new dependencies ...");
+await $`npm install -D commitlint-plugin-jira-rules commitlint-config-jira`;
+
+console.log(printStep(), "Updating configuration ...");
+fs.writeFileSync(
+  "commitlint.config.js",
+  `module.exports = {
+  plugins: ["commitlint-plugin-jira-rules"],
+  extends: ["jira"],
+};`
+);
